a598081a3dc8cb53d990a72b3d246d9225525aea
SelAdv1Mar-G4-Huong-Huynh <huonghuynh90@gmail.com>
2016-05-05 12:00:23 +0700

    update me

--- DemoProject/DemoTestCase.cs
+++ DemoProject/DemoTestCase.cs
@@ -15,5 +15,15 @@ namespace DemoProject
         {
             Console.WriteLine("Huong Huynh");
         }
+        public void Member1_TestMethod1()
+        {
+            Console.WriteLine("Hello member 2!");
+        }
+        public void Greeting_TestCase()
+        {
+            Console.WriteLine("Hello member 2!");
+            Console.WriteLine("jdfhgdfjkgkdf");
+            Console.WriteLine("I am member 1!");
+        }
     }
 }
